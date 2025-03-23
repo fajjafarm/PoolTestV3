05636de55d39e76f29d601b447a7826109fc86b6
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-03-23 21:08:01 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -115,10 +115,12 @@ Route::post('/pooltest', [PoolTestController::class, 'store'])->name('pooltest.s
 Route::get('/dashboards/pooltests', [PoolTestController::class, 'pooltests'])->name('pooltests');
 
 
+Route::group(['prefix' => '/', 'middleware' => 'auth'], function () {
     Route::get('', [RoutingController::class, 'index'])->name('root');
     Route::get('/home', fn()=>view('dashboards.index'))->name('home');
     Route::get('{first}/{second}/{third}', [RoutingController::class, 'thirdLevel'])->name('third');
     Route::get('{first}/{second}', [RoutingController::class, 'secondLevel'])->name('second');
     Route::get('{any}', [RoutingController::class, 'root'])->name('any');
+});
 
     Route::get('/sidebar-menu', [SidebarController::class, 'getPoolMenu'])->name('sidebar.menu');
